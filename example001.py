bd7da9033c675c1b5651939ea4bca79824ec3465
fenwk0 <karl.fenwick@gmail.com>
2017-05-17 07:39:57 +0100

    Added a second time decorator

--- example001.py
+++ example001.py
@@ -1,20 +1,41 @@
+import timeit
+
 __author__ = 'fenwk0'
 # This is our decorator
 # taken from
 # https://ains.co/blog/things-which-arent-magic-flask-part-1.html
+# see also
+
+# https://www.thecodeship.com/patterns/guide-to-python-function-decorators/
+
+
+def time_decorator(f):
+    # This is the new function we're going to return
+    # This function will be used in place of our original definition
+    def wrapper():
+        start = timeit.default_timer()
+
+        print("Start Time: ", start)
+        f()
+        print("Time taken: ", timeit.default_timer() - start)
+
+    return wrapper
+
 
 def simple_decorator(f):
     # This is the new function we're going to return
     # This function will be used in place of our original definition
     def wrapper():
-        print ("Entering Function")
+        print("Entering Function")
         f()
-        print ("Exited Function")
+        print("Exited Function")
 
     return wrapper
 
+
+@time_decorator
 @simple_decorator
 def hello():
-    print ("Hello World")
+    print("Hello World")
 
 hello()
